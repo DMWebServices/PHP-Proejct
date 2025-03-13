88a63335a12493cbe2f442bc57ee284a3325f309
DM Web Services <138100708+DMWebServices@users.noreply.github.com>
2025-03-13 15:08:50 +0000

    Mobile Changes Started
    Mobile Changes Started

--- index.php
+++ index.php
@@ -3,76 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Real Estate Agents</title>
+    <title>Job Listings</title>
     <link rel="icon" type="image/x-icon" href="assets/logo/fav-icon.png">
 
     <!-- FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Link -->
     <link rel="stylesheet" href="style.css">
 
 </head>
 <body>
-    
-    <!-- Site Wrapper -->
-    <div class="site-wrapper">
+       
         <!-- Header --> 
         <div class="header">
-            <!-- Logo -->
-            <img class="logo" src="assets/logo/logo.png" alt="" />
-            <!-- Filter -->
-            <form class="filter" action="">
-                <div class="filter--fields">
-                    <div class="">
-                        <label for="">What Job?</label>
-                            <input type="text" name="" id="">
-                    </div>
-                    <div>
-                        <label for="">Where?</label>
-                            <input type="text" name="" id="">
-                    </div> 
-                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>    
-            </form>
-            <!-- Account -->
-            <div>
+            <!-- Container -->
+            <div class="container">
+                <!-- Logo -->
+                <img class="logo" src="assets/logo/logo.png" alt="" />
+                <!-- Filter -->
+                <form class="filter" action="">
+                    <div class="filter--fields">
+                        <div class="filter--input">
+                            <label for="">What Job?</label>
+                                <input type="text" placeholder="Your Dream Job?" name="" id="">
+                        </div>
+                        <div class="filter--input">
+                            <label for="">Where?</label>
+                                <input type="text" placeholder="Your Location?" name="" id="">
+                        </div> 
+                        <button class="filter--btn" type="submit"><i class="filter--icon fa-solid fa-magnifying-glass"></i></button>
+                    </div>    
+                </form>
+                <!-- Account -->
+                <div>
+                    <i class="acc-icon fa-solid fa-circle-user"></i>
+                </div>
+            </div>
+        <!-- /Container -->
+        </div>    
+
+         <!-- Mobile Header --> 
+         <div class="mobile--header">
+            <!-- Container -->
+            <div class="container">
+                <!-- Search -->
+                <i class="search--icon fa-solid fa-magnifying-glass"></i>
+                <!-- Acc -->        
                 <i class="acc-icon fa-solid fa-circle-user"></i>
             </div>
-        </div>
-
-        <!-- Category Slider --> 
-        <div class="">
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </div>
-
-        <!-- House Cards --> 
-        <div class="">
-            <div class="cards">
-                <div>
-                    <h3>Test</h3>
-                    <p>Description</p>
-                    <button>Learn More</button>
-                </div>           
-            </div>
-        </div>
-    </div>        
-    <!-- /Site Wrapper Ends -->
-
-    <!-- Footer --> 
-    <div class="">
-        <div class="">
-            <div>
-                <h3>Test</h3>
-                <p>Description</p>
-                <button>Learn More</button>
-            </div>           
-        </div>
-    </div>
+        <!-- /Container -->
+        </div>    
         
 </body>
 </html>
