b74beb22d08196673fd6eef6d798c3caed0daebf
Daniel Mobbs <dan@danmobbs.co>
2025-03-13 08:48:31 +0000

    Beginning of project, HTML/CSS Files setup

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Estate Agents</title>
+    <link rel="icon" type="image/x-icon" href="assets/logo/fav-icon.png">
+
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <!-- Link -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+    
+    <!-- Site Wrapper -->
+    <div class="site-wrapper">
+        <!-- Header --> 
+        <div class="header">
+            <!-- Logo -->
+            <img class="logo" src="assets/logo/logo.png" alt="" />
+            <!-- Filter -->
+            <form class="filter" action="">
+                <div class="filter--fields">
+                    <div class="">
+                        <label for="">What Job?</label>
+                            <input type="text" name="" id="">
+                    </div>
+                    <div>
+                        <label for="">Where?</label>
+                            <input type="text" name="" id="">
+                    </div> 
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>    
+            </form>
+            <!-- Account -->
+            <div>
+                <i class="acc-icon fa-solid fa-circle-user"></i>
+            </div>
+        </div>
+
+        <!-- Category Slider --> 
+        <div class="">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+
+        <!-- House Cards --> 
+        <div class="">
+            <div class="cards">
+                <div>
+                    <h3>Test</h3>
+                    <p>Description</p>
+                    <button>Learn More</button>
+                </div>           
+            </div>
+        </div>
+    </div>        
+    <!-- /Site Wrapper Ends -->
+
+    <!-- Footer --> 
+    <div class="">
+        <div class="">
+            <div>
+                <h3>Test</h3>
+                <p>Description</p>
+                <button>Learn More</button>
+            </div>           
+        </div>
+    </div>
+        
+</body>
+</html>
+
+
